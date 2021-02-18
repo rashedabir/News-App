0e2f39d5f9f31725c8da7a11002c24552476028c
Rashed Khan <abir97bd@gmail.com>
2021-02-18 19:31:21 +0600

    first commit

--- src/NewsContext.jsx
+++ src/NewsContext.jsx
@@ -5,13 +5,11 @@ export const NewsContext = createContext();
 
 export const NewsContextProvider = (props) => {
   const [data, setData] = useState([]);
-  const apiKey = "016efbce20fb4e208b00ca29d9662dee";
+  const apiKey = "http://newsapi.org/v2/everything?q=tesla&from=2021-01-18&sortBy=publishedAt&apiKey=016efbce20fb4e208b00ca29d9662dee" || process.env.URL  ;
 
   useEffect(() => {
     axios
-      .get(
-        `http://newsapi.org/v2/everything?q=tesla&from=2021-01-18&sortBy=publishedAt&apiKey=${apiKey}`
-      )
+      .get(apiKey)
       .then((result) => setData(result.data))
       .catch((error) => console.log(error));
   }, []);
